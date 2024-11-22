b6057419812cf9ba398fffb5626517e0fdff494c
christiansen <andreschristiansen@proton.me>
2024-11-22 23:45:39 +0100

    m

---
+++ R/is.dfonly.R
@@ -0,0 +1,23 @@
+#' Is it a data frame and only a data frame?
+#'
+#'
+#' @inheritParams is.TF
+#' @inheritSection is.TF value
+#'
+#'
+#' @examples
+#' x <- as.data.frame(matrix(1:4,2,2))
+#' is.dfonly(x)
+#'
+#' class(x) <- c("tbl_df", "tbl", "data.frame")
+#' x
+#' is.dfonly(x)
+#'
+#' @export
+
+is.dfonly <- function(x){
+
+  is.data.frame(x)&&length(class(x))==1
+
+}
+
